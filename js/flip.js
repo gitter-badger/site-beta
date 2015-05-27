3a2b68e0538347c588c77ec44bdb5e69fabb9239
Joe Azar-Williams <flatflop@gmail.com>
2015-05-26 21:42:30 -0400

    Update flip.js

--- js/flip.js
+++ js/flip.js
@@ -1,8 +1,9 @@
-var el = document.getElementById('sign-up');
-			el.onclick = flip;
+var el1 = document.getElementById('sign-up');
+el1.onclick = flip;
+var el2 = document.getElementById('flip-back');
+el2.onclick = flip;
 
-
-			function flip() {
-			  document.querySelector("#card").classList.toggle("flip");
-			  return false;
-			}
+function flip() {
+	document.querySelector("#card").classList.toggle("flip");
+	return false;
+	}
